8b76003f8f354259dd90691ebd6d2aed74ac4e7d
Kateryna Kovalchuk <kateryna.kovalchuk@verygoodsecurity.com>
2021-11-19 23:57:06 +0200

    L3 complete HW

--- L3/L3HW3.py
+++ L3/L3HW3.py
@@ -143,3 +143,95 @@ def my_func7(any_number):
                 print("This number is NOT a palindrome.")
     else:
         print("The value you've entered is not a number.")
+
+
+# 12.
+def my_func8(a, b):
+    if type(a) == int and type(b) == int and a > b:
+        if a % b == 0:
+            print(f"{b} is a divider for {a}.")
+        else:
+            print(f"{b} is not a divider for {a}.")
+    else:
+        print("You gave incorrect arguments to the function")
+
+
+# 13.
+# Small comment here, that the task itself could have been set in different example with the same logic.
+
+
+def my_func9(a, b, d):
+    if a == b:  #if the window is a square
+        if d <= (a - 2):  #if window is a square, then a == b
+            return True
+        else:
+            return False
+    elif b > a:  #if the window is rectangle
+        if d <= (a -2):
+            return True
+        else:
+            return False
+    else:  #if a > b
+        if d <= (b - 2):
+            return True
+        else:
+            return False
+
+
+# 14.
+import math
+
+def my_func10(a, b, c):
+    a1 = float(a)
+    b1 = float(b)
+    c1 = float(c)
+
+    square_root = []
+
+    d = b1 ** 2 - 4 * a1 * c1
+    
+    if a != 0:
+        if d > 0:
+            x1 = (-b1 + math.sqrt(d)) / (2 * a1)
+            x2 = (-b1 - math.sqrt(d)) / (2 * a1)
+            square_root.extend([round(x1, 4), round(x2, 4)])
+            return square_root
+        elif d == 0:
+            x = -b1 / (2 * a1)
+            square_root.extend([round(x, 4)])
+            return square_root
+        else:
+            return square_root
+    else:
+        print("This is not a square equation")
+
+
+# 15.
+def my_func11(x):
+    return x ** 2
+
+
+# 16.
+def my_func12(x):
+    return x ** 2 + 5
+
+
+# 17.
+import datetime
+
+def my_func13(day, month, year):
+    try:
+        check_date = datetime.datetime(year, month, day)
+        print("Yes")
+    except ValueError:
+        print("No")
+
+
+# 18.
+def my_func14(n):
+    if 1 < n <= 99 and isinstance(n, int) is True:
+        print(f"I'm {n} years old")  #plural
+    elif n == 1:
+        print(f"I'm {n} year old")  #singular
+    else:
+        print("You've entered incorrect value in the function")
